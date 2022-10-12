d0f5c2b9ee3b0061067cb0858fcdf7c5190b9bb5
lutfi5853 <lutfi5853@gmail.com>
2022-10-12 23:24:41 +0700

    Add project files.

---
+++ Demo.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "RenderEngine.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <SOIL/SOIL.h>
+
+class Demo :
+	public RenderEngine
+{
+public:
+	Demo();
+	~Demo();
+private:
+	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2, VBO3, VAO3, EBO3, texture3, VBO4, VAO4, EBO4, texture4;
+	float angle = 0, viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
+	virtual void Init();
+	virtual void DeInit();
+	virtual void Update(double deltaTime);
+	virtual void Render();
+	virtual void ProcessInput(GLFWwindow* window);
+	void BuildColoredCube();
+	void BuildColoredPlane();
+	void BuildHeadRotate();
+	void BuildTailRotate();
+	void DrawColoredCube();
+	void DrawColoredPlane();
+	void DrawHeadRotate();
+	void DrawTailRotate();
+	void MoveCamera(float speed);
+	void StrafeCamera(float speed);
+	void RotateCamera(float speed);
+	void InitCamera();
+};
